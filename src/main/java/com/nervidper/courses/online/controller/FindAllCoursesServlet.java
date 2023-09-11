2d0d2b9bbf5d8747f1096ecd29de6901348cd38b
Nerea V <nerea.vidal.perez.dev@gmail.com>
2023-09-11 18:46:33 +0200

    Find all courses sevlet

---
+++ src/main/java/com/nervidper/courses/online/controller/FindAllCoursesServlet.java
@@ -0,0 +1,39 @@
+package com.nervidper.courses.online.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.nervidper.courses.online.facade.CoursesDelegate;
+import com.nervidper.courses.online.facade.CoursesFacade;
+import com.nervidper.courses.online.model.Course;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+
+@WebServlet("/FindAllCourses")
+public class FindAllCoursesServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		CoursesDelegate facade = new CoursesFacade();
+		List<Course> courses = facade.findAllCourses();
+		ObjectMapper mapper = JsonMapper.builder()
+                .addModule(new JavaTimeModule())
+                .build();
+		String json = mapper.writeValueAsString(courses);
+		System.out.println(json);
+		response.setContentType("application/json;charset=utf-8");
+		PrintWriter exit = response.getWriter();
+		exit.write(json);
+		exit.close();	
+	}
+
+}
