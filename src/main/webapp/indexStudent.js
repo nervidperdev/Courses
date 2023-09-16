82352beadd8c203697ac444c5ccb1e4589e38452
Nerea <nerea.vidal.dev@gmail.com>
2023-09-16 18:16:02 +0200

    Add input hidden

--- src/main/webapp/indexStudent.js
+++ src/main/webapp/indexStudent.js
@@ -19,7 +19,8 @@ function printCourses(courses) {
 			<div class="courseCard">
 				<img src="https://images.unsplash.com/photo-1501504905252-473c47e087f8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=320&q=80"/>
 				<h4>${course.name}</h4>
-				<input type="submit" value="MATRICULARSE" />
+				<input type="button" value="MATRICULARSE" onclick="enrollInCourse()"/>
+				<input type="hidden" id="courseId" value="${course.courseId}"/>
 			</div>
 		`;
 	}
@@ -28,4 +29,16 @@ function printCourses(courses) {
 	divResult.innerHTML = resultHtml;
 }
 
-
+function enrollInCourse() {
+	let courseIdInput = document.getElementById("courseId")
+		axios.post(
+			"EnrollCourse",
+			null,
+			{ params: { courseId : courseIdInput.value}})
+			.then(function(response) {
+				printCourses(response.data);
+			}).catch(function(error) {
+				console.log(error);
+			});
+	
+}
